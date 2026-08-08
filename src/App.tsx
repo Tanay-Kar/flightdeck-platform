3ca75839464c1d27d07205f3ab687af9380cb117
Tanay Kar <93914273+Tanay-Kar@users.noreply.github.com>
2026-08-08 15:41:31 +0530

    feat: AnnButton framing done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import KorryBtn from "./components/AnnButton/AnnButton"
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold text-center mt-10">
+      <h1 className="text-6xl font-bold text-center mt-10 text-mauve-300">
         Flightdeck Platform
       </h1>
+
+      <h3 className="text-2xl font-bold text-center mt-20 text-mist-300">
+        Annunciator Button Test
+      </h3>
+      <div className="flex items-center justify-center mx-auto my-10 w-50">
+        <KorryBtn />
+      </div>
     </>
   )
 }
---
+++ src/components/AnnButton/AnnButton.tsx
@@ -0,0 +1,11 @@
+
+export default function AnnButton() {
+    return (
+        <div className="w-full aspect-square bg-[#202020] rounded-lg p-2">
+            <div className="size-full bg-[#161616] [box-shadow:inset_0_0_4px_rgba(2,2,2,0.25),0_0_4px_5px_rgba(32,32,32,0.60)] rounded-md p-4">
+                <div className="size-full bg-[#131313] [box-shadow:inset_0_0_4px_rgba(64,64,64,0.25),0_0_8px_10px_rgba(1,1,1,0.30)] rounded-xl p-2">
+                </div>
+            </div>
+        </div>
+    )
+}
